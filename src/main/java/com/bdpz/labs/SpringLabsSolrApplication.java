657b7e7671ca3c94a4b44689711ab1bdbf75379c
bzani <ubzani@gmail.com>
2022-10-15 14:32:44 +0100

    initial commit, spring starter project

---
+++ src/main/java/com/bdpz/labs/SpringLabsSolrApplication.java
@@ -0,0 +1,13 @@
+package com.bdpz.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabsSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabsSolrApplication.class, args);
+	}
+
+}
